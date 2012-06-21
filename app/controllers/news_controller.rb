800a2c0f8571cf1609f0662e483b47a66deeac64
Younghun Choi <choi0hun@gmail.com>
2012-06-21 21:12:18 +0900

    delete all files have been deleted from file system

--- app/controllers/news_controller.rb
+++
@@ -1,9 +0,0 @@
-class NewsController < ApplicationController
-
-  respond_to :html
-  respond_to :json
-  def index
-    @news = New.all
-    respond_with @news
-  end
-end
--- db/migrate/20120226154914_create_news.rb
+++
@@ -1,9 +0,0 @@
-class CreateNews < ActiveRecord::Migration
-  def change
-    create_table :news do |t|
-      t.references :informable, :polymorphic => true
-
-      t.timestamps
-    end
-  end
-end
